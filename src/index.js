f137b026a0f5ef1298646e67f9a373a93686492a
Dennis Akagha <d.akagha20@gmail.com>
2022-02-08 21:45:45 +0100

    Project setup

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import _ from 'lodash';
 import css from './styles.css';
+import displayData from './displayData.js';
+
+// display the data
+displayData();
---
+++ src/scoreData.js
@@ -0,0 +1,9 @@
+const scoreData = [
+  { name: 'Dennia', score: '25' },
+  { name: 'Peter', score: '50' },
+  { name: 'Omoniyi', score: 100 },
+  { name: 'Dennis', score: 25 },
+  { name: 'Dennis', score: 25 },
+];
+
+export default scoreData;
