b1e8f29951a37740e2475f228c00b6c90dac57d8
Dennis Akagha <d.akagha20@gmail.com>
2022-02-10 22:44:56 +0100

    Fix bug - form not submitting

--- src/displayData.js
+++ src/displayData.js
@@ -2,6 +2,7 @@ import { ul } from './selectors.js';
 
 const displayData = (data) => {
   const scoreData = data.result;
+  scoreData.sort((firstItem, secondItem) => firstItem.score - secondItem.score);
   scoreData.forEach((data) => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `${data.user}<span>${data.score}</span>`;
